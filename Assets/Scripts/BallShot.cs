7a8f5f244dbc2d55725cbbd016902329a191dc68
Maeda jo <133735669+maaeda@users.noreply.github.com>
2024-06-12 14:35:49 +0900

    発射君

---
+++ Assets/Scripts/BallShot.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallShot : MonoBehaviour
+{
+    [SerializeField] GameObject sphere;
+    [SerializeField] GameObject childObj;
+    private float speed = 1400;
+
+    void Start()
+    {
+        childObj = transform.GetChild(0).gameObject;
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown("space"))
+        {
+            GameObject ball =  (GameObject)Instantiate(sphere, childObj.transform.position, Quaternion.identity);
+            Rigidbody ballRigidbody = ball.GetComponent<Rigidbody>();
+            ballRigidbody.AddForce(transform.forward * speed);
+        }
+
+        if(Input.GetKey("right"))
+        {
+            transform.Rotate(0,1,0);
+        }
+
+        if(Input.GetKey("left"))
+        {
+            transform.Rotate(0,-1,0);
+        }
+
+        if(Input.GetKey("up"))
+        {
+            transform.Rotate(-1,0,0);
+        }
+
+        if(Input.GetKey("down"))
+        {
+            transform.Rotate(1,0,0);
+        }
+    }
+}
