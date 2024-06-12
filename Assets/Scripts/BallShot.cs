85e08252e1d0d07820885c1da589cbf3f42a0beb
Maeda jo <133735669+maaeda@users.noreply.github.com>
2024-06-12 15:51:31 +0900

    衝突

--- Assets/Scripts/BallShot.cs
+++ Assets/Scripts/BallShot.cs
@@ -17,6 +17,7 @@ public class BallShot : MonoBehaviour
     {
         if(Input.GetKeyDown("space"))
         {
+
             GameObject ball =  (GameObject)Instantiate(sphere, childObj.transform.position, Quaternion.identity);
             Rigidbody ballRigidbody = ball.GetComponent<Rigidbody>();
             ballRigidbody.AddForce(transform.forward * speed);
