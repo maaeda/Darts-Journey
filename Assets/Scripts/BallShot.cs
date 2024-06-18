46aef296484b431abd4073ccd53e13bab3aa8a68
maeda jo <133735669+maaeda@users.noreply.github.com>
2024-06-18 20:49:46 +0900

    マウスホイールで視点リセット

--- Assets/Scripts/BallShot.cs
+++ Assets/Scripts/BallShot.cs
@@ -1,16 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class BallShot : MonoBehaviour
 {
     [SerializeField] GameObject sphere;
     [SerializeField] GameObject childObj;
     private float speed = 1300;
+    private Vector3 initialPosition;
+    private Quaternion initialRotation;
 
     void Start()
     {
         childObj = transform.GetChild(0).gameObject;
+        initialPosition = transform.position;
+        initialRotation = transform.rotation;
     }
 
     void Update()
@@ -29,13 +34,18 @@ public class BallShot : MonoBehaviour
             ballRigidbody.AddForce(transform.forward * speed);
         }
 
+        if (Input.GetMouseButtonDown(2))
+        {
+            transform.position = initialPosition;
+            transform.rotation = initialRotation;
+        }
+
         float mx = Input.GetAxis("Mouse X");//カーソルの横の移動量を取得
         float my = Input.GetAxis("Mouse Y");//カーソルの縦の移動量を取得
         transform.Rotate(my*-1,mx,0);
         if (Mathf.Abs(mx) > 0.001f) // X方向に一定量移動していれば横回転
         {
             transform.RotateAround(transform.position, Vector3.up, mx); // 回転軸はplayerオブジェクトのワールド座標Y軸
-
         }
 
         if(Input.GetKey("right"))
